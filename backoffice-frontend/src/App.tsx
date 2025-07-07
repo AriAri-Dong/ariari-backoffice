2b85f55af6ada43f4e39c00179213bf29fb98705
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-07-07 14:28:13 +0900

    feat: Add routes

--- backoffice-frontend/src/App.tsx
+++ backoffice-frontend/src/App.tsx
@@ -1,8 +1,39 @@
 import './index.css';
-import './main';
+import LeftMenu from './components/layout/leftMenu';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
-  return <>Frontend Backoffice</>;
+  return (
+    <BrowserRouter>
+      <div className='flex h-screen'>
+        <LeftMenu />
+        <div className='flex-1 overflow-y-auto p-6'>
+          <Routes>
+            <Route
+              path='/'
+              element={<div>홈</div>}
+            />
+            <Route
+              path='/operate'
+              element={<div>운영</div>}
+            />
+            <Route
+              path='/report'
+              element={<div>신고</div>}
+            />
+            <Route
+              path='/data'
+              element={<div>데이터 조회/삭제</div>}
+            />
+            <Route
+              path='/crud'
+              element={<div>CRUD</div>}
+            />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
