0354c48d6b99a651bfe22a43ccc1702f081462c5
yeoniii20 <commetoi_yeoni@naver.com>
2025-07-20 02:06:03 +0900

    feat: Create paginatedTable component

---
+++ backoffice-frontend/src/components/paginatedTable.tsx
@@ -0,0 +1,48 @@
+import { useMemo, useState } from 'react';
+import Pagination from './pagination';
+import Table from './table';
+import type { Column } from '../types/table';
+
+type PaginatedTableProps<T> = {
+  columns: Column<T>[];
+  data: T[];
+  pageSize?: number;
+  rowKey?: keyof T | ((row: T) => string | number);
+  className?: string;
+};
+
+export default function PaginatedTable<T>({
+  columns,
+  data,
+  pageSize = 10,
+  rowKey,
+  className = '',
+}: PaginatedTableProps<T>) {
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const totalPages = Math.ceil(data.length / pageSize);
+
+  const pagedData = useMemo(() => {
+    const start = (currentPage - 1) * pageSize;
+    return data.slice(start, start + pageSize);
+  }, [data, currentPage, pageSize]);
+
+  return (
+    <div className={`w-full ${className}`}>
+      <div className='h-[685px]'>
+        <Table
+          columns={columns}
+          data={pagedData}
+          rowKey={rowKey}
+        />
+      </div>
+
+      {totalPages > 1 && (
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={setCurrentPage}
+        />
+      )}
+    </div>
+  );
+}
