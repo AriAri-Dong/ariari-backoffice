7f034bd725176f690a13083c077313970352fe76
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-07-22 15:01:48 +0900

    style: Update smallBtn.tsx

--- backoffice-frontend/src/components/button/basicBtn/smallBtn.tsx
+++ backoffice-frontend/src/components/button/basicBtn/smallBtn.tsx
@@ -11,7 +11,7 @@ import type { ButtonWithTextProps } from '../../../types/button';
 const SmallBtn = ({ title, onClick, round = false, className }: ButtonWithTextProps) => {
   return (
     <button
-      className={`text-mobile_body2_sb md:text-body1_sb active:bg-primary_hover bg-primary text-background md:hover:bg-primary_hover md:active:bg-primary_pressed cursor-pointer px-[18px] py-2 md:px-[22px] md:py-[13px] ${round ? 'rounded-full' : 'rounded-lg'} ${className}`}
+      className={`text-body1_sb active:bg-primary_hover bg-primary text-background md:hover:bg-primary_hover md:active:bg-primary_pressed cursor-pointer px-[22px] py-[13px] whitespace-nowrap ${round ? 'rounded-full' : 'rounded-lg'} ${className}`}
       onClick={onClick}
     >
       {title}
