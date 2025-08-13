7feb905b3b067fd487f23f1f998722e8d4698bc9
winterholic <iuholic83@naver.com>
2025-08-13 15:02:16 +0900

    Fix : Member엔티티수정 및 IndexController추가

---
+++ backoffice-backend/src/main/java/com/ariari/ariari/commons/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.ariari.ariari.commons.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    private static final String INDEX = "index";
+
+    @GetMapping(value = {"/"})
+    public String root() {
+        return INDEX;
+    }
+
+}
--- backoffice-backend/src/main/java/com/ariari/ariari/domain/member/Member.java
+++ backoffice-backend/src/main/java/com/ariari/ariari/domain/member/Member.java
@@ -1,15 +1,25 @@
 package com.ariari.ariari.domain.member;
 
 import com.ariari.ariari.commons.entity.LogicalDeleteEntity;
+import com.ariari.ariari.commons.entity.report.Report;
 import com.ariari.ariari.commons.pkgenerator.CustomPkGenerate;
+import com.ariari.ariari.domain.club.activity.ClubActivity;
+import com.ariari.ariari.domain.club.activity.comment.ClubActivityComment;
+import com.ariari.ariari.domain.club.activity.comment.like.ClubActivityCommentLike;
+import com.ariari.ariari.domain.club.activity.like.ClubActivityLike;
 import com.ariari.ariari.domain.club.bookmark.ClubBookmark;
 import com.ariari.ariari.domain.club.clubmember.ClubMember;
 import com.ariari.ariari.domain.club.event.attendance.Attendance;
 import com.ariari.ariari.domain.club.notice.ClubNotice;
+import com.ariari.ariari.domain.club.passreview.PassReview;
+import com.ariari.ariari.domain.club.question.ClubQuestion;
+import com.ariari.ariari.domain.club.review.ClubReview;
 import com.ariari.ariari.domain.member.alarm.MemberAlarm;
 import com.ariari.ariari.domain.member.block.Block;
 import com.ariari.ariari.domain.member.enums.ProfileType;
+import com.ariari.ariari.domain.member.point.PointHistory;
 import com.ariari.ariari.domain.recruitment.apply.Apply;
+import com.ariari.ariari.domain.recruitment.apply.temp.ApplyTemp;
 import com.ariari.ariari.domain.recruitment.bookmark.RecruitmentBookmark;
 import com.ariari.ariari.domain.school.School;
 import jakarta.persistence.*;
@@ -50,6 +60,9 @@ public class Member extends LogicalDeleteEntity {
     @Column(length = 20)
     private ProfileType profileType;
 
+    @Column(nullable = false)
+    private boolean isSuperAdmin;
+
     @ElementCollection(fetch = FetchType.LAZY)
     private Set<GrantedAuthority> authorities = new HashSet<>();
 
@@ -61,6 +74,32 @@ public class Member extends LogicalDeleteEntity {
     @JoinColumn(name = "school_id")
     private School school;
 
+    /* -------------------- reverse mapping  -------------------- */
+
+    @OneToMany(mappedBy = "member")
+    private List<PassReview> passReviews = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member")
+    private List<ClubReview> clubReviews = new ArrayList<>();
+
+    @OneToMany(mappedBy = "reporter")
+    private List<Report> reports = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member")
+    private List<ClubNotice> clubNotices = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member")
+    private List<ClubQuestion> clubQuestions = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member")
+    private List<ClubActivity> clubActivities = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member")
+    private List<ClubActivityComment> clubActivityComments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member")
+    private List<ClubMember> clubMembers = new ArrayList<>();
+
     @OneToMany(mappedBy = "member", cascade = CascadeType.REMOVE)
     private List<MemberAlarm> memberAlarms = new ArrayList<>();
 
@@ -71,9 +110,6 @@ public class Member extends LogicalDeleteEntity {
     private List<Block> blockeds = new ArrayList<>();
 
     @OneToMany(mappedBy = "member", cascade = CascadeType.REMOVE)
-    private List<ClubMember> clubMembers = new ArrayList<>();
-
-    @OneToMany(mappedBy = "member", cascade = CascadeType.REMOVE)
     private List<ClubBookmark> clubBookmarks = new ArrayList<>();
 
     @OneToMany(mappedBy = "member", cascade = CascadeType.REMOVE)
@@ -82,16 +118,31 @@ public class Member extends LogicalDeleteEntity {
     @OneToMany(mappedBy = "member", cascade = CascadeType.REMOVE)
     private List<Apply> applys = new ArrayList<>();
 
+    @OneToMany(mappedBy = "member", cascade = CascadeType.REMOVE)
+    private List<ApplyTemp> applyTemps = new ArrayList<>();
 
     @OneToMany(mappedBy = "member", cascade = CascadeType.REMOVE)
     private List<Attendance> attendances = new ArrayList<>();
 
-    @OneToMany(mappedBy = "member")
-    private List<ClubNotice> clubNotices = new ArrayList<>();
+    @OneToMany(mappedBy = "member", cascade = CascadeType.REMOVE)
+    private List<ClubActivityLike> clubActivityLikes = new ArrayList<>();
 
+    @OneToMany(mappedBy = "member", cascade = CascadeType.REMOVE)
+    private List<ClubActivityCommentLike> clubActivityCommentLikes = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.REMOVE)
+    private List<PointHistory> pointHistories = new ArrayList<>();
+
+    /* --------------------------------------------------- */
 
     public static Member createMember(Long kakaoId, String nickname) {
         Member member = new Member(kakaoId, nickname);
+        member.addAuthority(new SimpleGrantedAuthority("ROLE_USER"));
+        return member;
+    }
+
+    public static Member createMember(Long kakaoId, String nickname, ProfileType profileType) {
+        Member member = new Member(kakaoId, nickname, profileType);
         member.addAuthority(new SimpleGrantedAuthority("ROLE_USER"));
         return member;
     }
@@ -105,8 +156,18 @@ public class Member extends LogicalDeleteEntity {
         this.nickName = nickName;
     }
 
+    public Member(Long kakaoId, String nickName, ProfileType profileType) {
+        this.kakaoId = kakaoId;
+        this.nickName = nickName;
+        this.profileType = profileType;
+    }
+
     public void addAuthority(GrantedAuthority authority) {
         this.authorities.add(authority);
+    }
+
+    public void setKakaoIdNull() {
+        this.kakaoId = null;
     }
 
 }
