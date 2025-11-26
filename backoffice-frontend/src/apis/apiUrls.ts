c526ef16000d1da8a061597b3058ebc429a75426
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-11-26 17:24:07 +0900

    feat: Update apiUrls.ts

--- backoffice-frontend/src/apis/apiUrls.ts
+++ backoffice-frontend/src/apis/apiUrls.ts
@@ -6,7 +6,11 @@ export const DASHBOARD_CLUB = '/api/v1/dashboard/club/statistics';
 export const DASHBOARD_RANKING = '/api/v1/dashboard/club/ranking';
 
 // === 운영 ===
+// 약관
+export const TERMS = '/api/v1/terms';
 // 공지사항
 export const NOTICE = '/api/v1/notices';
 // FAQ
 export const FAQ = '/api/v1/faqs';
+// 알림전송 (시스템 알림)
+export const SYSTEM_ALARM = '/api/v1/notifications';
